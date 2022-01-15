edb395c2e85642e2f69ba90779b3d51d679dfbb8
dangngocthu <dnthu.20it@vku.udn.vn>
2022-01-15 10:13:24 +0700

    Lab2_Phan2

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -13,11 +13,21 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener{
             rollDice()
         }
+        rollDice()
     }
     private fun rollDice(){
         val dice=Dice(6)
         val diceRoll=dice.roll()
-        textView.text=diceRoll.toString()
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        imageView.setImageResource(drawableResource)
+        imageView.contentDescription=diceRoll.toString()
     }
 
 
